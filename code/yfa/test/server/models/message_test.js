37fa53fd58bd023aab2938a7944cf609f1b8bf19
Jim Schubert <james.schubert@gmail.com>
2014-09-03 21:38:18 -0400

    Test: Message#saveMessage

---
+++ code/yfa/test/server/models/message_test.js
@@ -0,0 +1,90 @@
+"use strict";
+var assert = require('assert'),
+    connection = require('../../db_common'),
+    Message = require('../../../src/models/message'),
+    User = require('../../../src/models/user'),
+    async = require('async');
+require('should');
+
+describe("Message", function() {
+    var db, users = [];
+
+    function createTestUser(name, done) {
+        var user = new User({
+            username: name,
+            firstName: name,
+            lastName: name,
+            email: name + "@example.com",
+            state: "offline"
+        });
+        user.save(function(err, doc){
+            return done(err, doc);
+        });
+    }
+
+    before(function () {
+        db = connection();
+    });
+
+    after(function (done) {
+        db.close(done);
+    });
+
+    beforeEach(function(done){
+        async.parallel([
+            async.apply(createTestUser,'jim'),
+            async.apply(createTestUser,'crystal')
+        ], function(err, results){
+            users = results;
+            done(err);
+        });
+    });
+
+    describe('saveMessage', function(){
+        it('should save an object without an attachment.', function(done){
+            Message.saveMessage({
+                to: users[0]._id,
+                from: users[1]._id,
+                body: 'hello'
+            }, function(err, result){
+                // this may be weird, but the result should be the 'to' user.
+                (err == null).should.be.true;
+                result.should.not.be.null;
+                result.should.be.an.object;
+                (result._id.equals(users[0]._id)).should.be.true;
+                result.messages.should.be.an.array;
+                result.messages.length.should.be.exactly(1);
+                done();
+            });
+        });
+
+        it('should save an object with an attachment.', function(done){
+            var att = { dataURI: 'data:image/png;base64,R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs=' };
+            Message.saveMessage({
+                to: users[0]._id,
+                from: users[1]._id,
+                body: 'hello',
+                attachment: att
+            }, function(err, result){
+                // this may be weird, but the result should be the 'to' user.
+                (err == null).should.be.true;
+                result.should.not.be.null;
+                result.should.be.an.object;
+                (result._id.equals(users[0]._id)).should.be.true;
+                result.messages.should.be.an.array;
+                result.messages.length.should.be.exactly(1);
+
+                Message.findOne({_id: result.messages[0] }, null, null, function(err, result){
+                    console.log(result);
+                    (err == null).should.be.true;
+                    result.should.not.be.null;
+                    result.should.be.an.object;
+                    result.body.should.equal('hello');
+                    result.attachment.should.be.an.object;
+                    (result.attachment.dataURI === undefined).should.be.true;
+                    done();
+                });
+            });
+        });
+    });
+});
