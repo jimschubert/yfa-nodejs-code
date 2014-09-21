454950ffbdabcb4cae3ea7f2232fdec28a93a883
Jim Schubert <james.schubert@gmail.com>
2014-09-21 15:28:41 -0400

    Authenticated api interaction

--- code/yfa/src/public/javascripts/app.js
+++ code/yfa/src/public/javascripts/app.js
@@ -1,12 +1,21 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'myApp.compiled','ui.bootstrap', 'ngRoute']).
-    config(['$routeProvider', function ($routeProvider) {
+    config([
+                 '$routeProvider','USER_CONTEXT',
+        function ($routeProvider , USER_CONTEXT) {
         "use strict";
 
-        $routeProvider.when('/', {
-            templateUrl: '/partials/index.html',
-            controller: 'IndexCtrl'
-        });
+        if(!USER_CONTEXT.id) {
+            $routeProvider.when('/', {
+                templateUrl: '/partials/index.html',
+                controller: 'IndexCtrl'
+            });
+        } else {
+            $routeProvider.when('/', {
+                templateUrl: '/partials/dashboard.html',
+                controller: 'DashboardCtrl'
+            });
+        }
 
         $routeProvider.otherwise({ redirectTo: '/' });
     }]);
--- code/yfa/src/public/javascripts/controllers.js
+++ code/yfa/src/public/javascripts/controllers.js
@@ -1,21 +1,56 @@
 (function(angular){
     'use strict';
 
+    // Node's implementation of util.inherits
+    function _extend(ctor, superCtor) {
+        ctor.super_ = superCtor;
+        ctor.prototype = Object.create(superCtor.prototype, {
+            constructor: {
+                value: ctor,
+                enumerable: false,
+                writable: true,
+                configurable: true
+            }
+        });
+    }
+
+    /**
+     * IndexCtrl handles unauthenticated index.
+     * @param $scope
+     * @constructor
+     */
+    function IndexCtrl($scope) {
+        $scope.alerts = [];
+        $scope.closeAlert = function(index) {
+            $scope.alerts.splice(index, 1);
+        };
+    }
+    IndexCtrl.$inject = ['$scope'];
+
+    /**
+     * DashboardCtrl handles authenticated index
+     * @param $scope
+     * @param Api
+     * @constructor
+     */
+    function DashboardCtrl($scope, Api) {
+        IndexCtrl.call(this, $scope);
+
+        Api.users.list().success(function(data){
+            console.log(data);
+        });
+    }
+    DashboardCtrl.$inject = ['$scope', 'Api'];
+
+    _extend(DashboardCtrl, IndexCtrl);
+
     /* Controllers */
     angular.module('myApp.controllers', ['myApp.compiled', 'ui.bootstrap'])
-        .controller('IndexCtrl', [
-                    '$scope',
-            function($scope) {
-                $scope.alerts = [];
-                $scope.closeAlert = function(index) {
-                    $scope.alerts.splice(index, 1);
-                };
-            }
-        ])
+        .controller('IndexCtrl', IndexCtrl)
+        .controller('DashboardCtrl', DashboardCtrl)
         .controller('NavigationCtrl', [
-                    '$scope','$location','USER_CONTEXT',
-            function($scope , $location , USER_CONTEXT){
-                console.log(USER_CONTEXT);
+                    '$scope','$location',
+            function($scope , $location){
                 $scope.isCurrent = function (me) {
                     var currentRoute = $location.path() || '/';
                     return me === currentRoute ? 'active' : '';
