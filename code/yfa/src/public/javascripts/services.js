1cdadc6ef646c900aa42317bd89ef67dc0a434e9
Jim Schubert <james.schubert@gmail.com>
2014-09-21 15:25:42 -0400

    Api interaction

--- code/yfa/src/public/javascripts/services.js
+++ code/yfa/src/public/javascripts/services.js
@@ -4,4 +4,24 @@
     /* Services */
     angular.module('myApp.services', []).
         value('version', '0.1');
+
+    angular.module('myApp.services')
+        .service('Api', [
+                    '$http',
+            function($http){
+                var apiBase = '/api/v1';
+                return {
+                    users: {
+                        list: function (options){
+                            return $http({
+                                url: apiBase + '/users',
+                                params: options,
+                                method: 'GET',
+                                cache: false
+                            });
+                        }
+                    }
+                };
+            }
+        ]);
 })(angular);
