ff97a865a29a1beae96b292264c5747bbb16f99a
James Schubert <james.schubert@gmail.com>
2017-11-14 23:04:23 -0500

    WIP: Upgrading Angular, Bootstrap, ui-bootstrap

--- code/yfa/src/public/javascripts/app.js
+++ code/yfa/src/public/javascripts/app.js
@@ -1,15 +1,21 @@
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ui.bootstrap']).
-    config(['$routeProvider', function ($routeProvider) {
-        'use strict';
+angular.module('myApp', [
+    'myApp.filters',
+    'myApp.services',
+    'myApp.directives',
+    'myApp.controllers',
+    'ui.bootstrap',
+    'ngRoute'
+]).config(['$routeProvider', function ($routeProvider) {
+    'use strict';
 
-        $routeProvider.when('/', {
-            templateUrl: '/partials/index.html',
-            controller: 'IndexCtrl'
-        });
+    $routeProvider.when('/', {
+        templateUrl: '/partials/index.html',
+        controller: 'IndexCtrl'
+    });
 
-        $routeProvider.otherwise({ redirectTo: '/' });
-    }]);
+    $routeProvider.otherwise({ redirectTo: '/' });
+}]);
 
 (function () {
     'use strict';
